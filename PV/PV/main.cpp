56d43b23eac6bf6b0308e08a64480f785cc2b514
Micah Rust <micahtim@gmail.com>
2018-05-11 19:32:14 -0700

    I am about to make the graphics Pipeline! YEAH!!!!

--- PV/PV/main.cpp
+++ PV/PV/main.cpp
@@ -110,6 +110,7 @@ private:
 	std::vector<VkImage> swapChainImages;
 	VkFormat swapChainImageFormat;
 	VkExtent2D swapChainExtent;
+	std::vector<VkImageView> swapChainImageViews;
 
 	QueueFamilyIndices selectedQueueFamily;
 	float queuePriority = 1.0f;
@@ -152,6 +153,8 @@ private:
 		createSurface();
 		pickPhysicalDevice();
 		createLogicalDevice();
+		createSwapChain();
+		createSwapChainImageViews();	
 	}
 
 	void createInstance()
@@ -456,7 +459,33 @@ private:
 			swapChainExtent = resoulationExtent;
 		}
 	}
+	void createSwapChainImageViews()
+	{
+		swapChainImageViews.resize(swapChainImages.size());
 
+		for (size_t i = 0; i < swapChainImages.size(); ++i)
+		{
+			VkImageViewCreateInfo createInfo = {};
+			createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image = swapChainImages[i];
+
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			createInfo.format = swapChainImageFormat;
+
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+			
+			createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			createInfo.subresourceRange.baseMipLevel = 0;
+			createInfo.subresourceRange.levelCount = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0; // for sterioscopic rendering (VR)
+			createInfo.subresourceRange.layerCount = 1;
+
+			PV_VK_RUN(vkCreateImageView(device, &createInfo, allocnullptr, &swapChainImageViews[i]));
+		}
+	}
 	// Helper functions
 	bool isDeviceSuitable(VkPhysicalDevice device)
 	{
@@ -783,6 +812,13 @@ private:
 	{
 		// vulkan cleanup
 		{
+			// cleanup anyu imageviews connected to the swap chain
+			for (auto imageView : swapChainImageViews)
+			{
+				vkDestroyImageView(device, imageView, allocnullptr);
+			}
+
+
 			// destroy swap chain
 			vkDestroySwapchainKHR(device, swapChain, nullptr);
 			swapChainImages.clear();
