0ad317168606bbc1e6497b46aa72d0723341df8b
Micah <micahtim@gmail.com>
2018-05-18 15:36:45 -0700

    Next is the renderPass! YEAH!

--- PV/PV/main.cpp
+++ PV/PV/main.cpp
@@ -127,6 +127,10 @@ private:
 	QueueFamilyIndices selectedQueueFamily;
 	float queuePriority = 1.0f;
 
+	// will reference this later, will probably make a special GraphicsPipeline holder which holds onto different references 
+	// and other details on a per graphics Pipeline basis
+	VkPipelineLayout pipelineLayout;
+
 	std::vector<const char*> validationLayers =
 	{
 		"VK_LAYER_LUNARG_standard_validation",
@@ -167,6 +171,7 @@ private:
 		createLogicalDevice();
 		createSwapChain();
 		createSwapChainImageViews();
+		createRenderPass();
 		createGraphicsPipeline();
 	}
 
@@ -499,6 +504,10 @@ private:
 			PV_VK_RUN(vkCreateImageView(device, &createInfo, allocnullptr, &swapChainImageViews[i]));
 		}
 	}
+	void createRenderPass()
+	{
+
+	}
 	void createGraphicsPipeline()
 	{
 		auto vertShaderCode = readBinaryFile("../SPV/vert.spv"); //@Speed
@@ -570,10 +579,87 @@ private:
 
 		// can be used for shadow mapping
 		rasterizer.depthBiasEnable = VK_FALSE;
-		rasterizer.depthBiasConstantFactor = 0.0f; // optional
-		rasterizer.depthBiasClamp = 0.0f; // optional
-		rasterizer.depthBiasSlopeFactor = 0.0f; // optional
+		rasterizer.depthBiasConstantFactor = 0.0f;	// optional
+		rasterizer.depthBiasClamp = 0.0f;			// optional
+		rasterizer.depthBiasSlopeFactor = 0.0f;		// optional
 
+		VkPipelineMultisampleStateCreateInfo multisampling = {};
+		multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+		multisampling.sampleShadingEnable = VK_FALSE;
+		multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+		multisampling.minSampleShading = 1.0f;			// Optional
+		multisampling.pSampleMask = nullptr;			// Optional
+		multisampling.alphaToCoverageEnable = VK_FALSE; // Optional
+		multisampling.alphaToOneEnable = VK_FALSE;		// Optional
+
+		// TODO VkPipelineDepthStencilStateCreateInfo = {};
+		// for 3D depth testing...?
+
+		const bool blendEnabled = false;
+		VkPipelineColorBlendAttachmentState colorBlendAttachment = {};
+		if (blendEnabled)
+		{
+			colorBlendAttachment.blendEnable = VK_TRUE;
+			colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA; // alpha channel determins blending
+			colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+			colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
+			colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+			colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+			colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
+		}
+		else
+		{
+			colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+			colorBlendAttachment.blendEnable = VK_FALSE; // disable colorBlend just makes finalColor = newColor
+			colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;		// Optional
+			colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO;	// Optional
+			colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;				// Optional
+			colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;		// Optional
+			colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;	// Optional
+			colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;				// Optional
+		}
+
+		VkPipelineColorBlendStateCreateInfo colorBlending = {};
+		colorBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+		if (blendEnabled)
+		{
+			colorBlending.logicOpEnable = VK_TRUE;
+			colorBlending.logicOp = VK_LOGIC_OP_COPY; //@BUG? not sure if this should be something else...
+		}
+		else
+		{
+			colorBlending.logicOpEnable = VK_FALSE;
+			colorBlending.logicOp = VK_LOGIC_OP_COPY; // Optional
+		}
+		colorBlending.attachmentCount = 1;
+		colorBlending.pAttachments = &colorBlendAttachment;
+		colorBlending.blendConstants[0] = 0.0f; // Optional
+		colorBlending.blendConstants[1] = 0.0f; // Optional
+		colorBlending.blendConstants[2] = 0.0f; // Optional
+		colorBlending.blendConstants[3] = 0.0f; // Optional
+
+		// Dynamic state can be changed without recreating the graphic Pipeline
+		VkDynamicState dynamicStates[] =
+		{
+			VK_DYNAMIC_STATE_VIEWPORT,
+			VK_DYNAMIC_STATE_LINE_WIDTH
+		};
+
+		VkPipelineDynamicStateCreateInfo dynamicState = {};
+		dynamicState.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+		dynamicState.dynamicStateCount = 2;
+		dynamicState.pDynamicStates = dynamicStates;
+
+		// make an empty pipeline layout for now. This will later have things like worldViewMatrix
+		// and other constants which are passed on a per graphics pipeline basis
+		VkPipelineLayoutCreateInfo pipelineLayoutInfo = {};
+		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+		pipelineLayoutInfo.setLayoutCount = 0; // Optional
+		pipelineLayoutInfo.pSetLayouts = nullptr; // Optional
+		pipelineLayoutInfo.pushConstantRangeCount = 0; // Optional
+		pipelineLayoutInfo.pPushConstantRanges = nullptr; // Optional
+
+		PV_VK_RUN(vkCreatePipelineLayout(device, &pipelineLayoutInfo, allocnullptr, &pipelineLayout));
 
 
 		vkDestroyShaderModule(device, vertShaderModule, allocnullptr);
@@ -917,6 +1003,9 @@ private:
 	{
 		// vulkan cleanup
 		{
+			// Cleanup empty PipelineLayout
+			vkDestroyPipelineLayout(device, pipelineLayout, allocnullptr);
+
 			// cleanup anyu imageviews connected to the swap chain
 			for (auto imageView : swapChainImageViews)
 			{
